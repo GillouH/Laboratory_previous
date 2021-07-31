0757319e44294bfe1a8dd0b6bf069c4cebce3088
Gillou <gilles.hubert97@gmail.com>
2021-07-31 02:30:39 +0200

    Create a user interface with TKinter for Python Client
    The python Client now manage server shutdown
    Fix display bug on Server for shutdown print message
    Add some Exception managment

--- Client/Client_Python.py
+++ Client/Client_Python.py
@@ -1,26 +1,194 @@
+from tkinter import NS, VERTICAL
+
+from tkinter import Tk, Widget, StringVar, Frame, Button, Label, Entry, Text, Scrollbar # Classes used
+from tkinter.font import Font
+from laboratoryTools.network import serverAddress, createSocket, TIMEOUT, STOP_SERVER
+from tkinter import NORMAL, DISABLED    # Widget State Constants
+from threading import Thread, get_ident
 from socket import socket
-from laboratoryTools.network import createSocket, serverAddress, serverAddressStr, checkInput, STOP_SERVER
+from select import select
+from tkinter import END # Index Constant for Text
+from tkinter import NSEW, NS    # Fill Direction Size Constant
+from tkinter import RIGHT   # Widget side display Constant
+from tkinter import VERTICAL    # Scrollbar Direction Constant
 
 
-STOP:str = "STOP"
+class ClientWindow(Tk):
+    BASE_TITLE:str = "Client"
+    CONNECTION:str = "Connection"
+    CONNECTING:str = "Connecting..."
+    CONNECTED:str = "Connected"
+    DISCONNECTION:str = "Disconnection"
+    DISCONNECTED:str = "Disconnected"
+    
+    def __init__(self):
+        super().__init__()
 
-server:socket = createSocket()
-connected:bool = False
-server.connect(serverAddress)
-print("Connected to the server at {}".format(serverAddressStr))
+        self.FONT:Font = Font(root=self, size=15)
+        self.serverConfigWidget:list[Widget] = []
+        self.isConnected:bool = False
 
-clientLoop:bool = True
-while clientLoop:
-    msgToSend:str = checkInput(prompt="Message to send to the server at {}:\n\t_".format(serverAddressStr))
-    if msgToSend == STOP:
-        clientLoop = False
-    else:
-        server.send(msgToSend.encode())
-        msgReceived:str = server.recv(1024).decode()
-        print("Message received from the server at {}:\n\t{}".format(serverAddressStr, msgReceived))
-        if msgToSend == STOP_SERVER and msgReceived == STOP_SERVER:
-            print("The server at {} is shutting down.".format(serverAddressStr))
-            clientLoop = False
+        self.portTextVariable:StringVar = StringVar()
+        self.portTextVariable.set(value=serverAddress[1])
+            
+        IPDefaultValueList:list[str] = serverAddress[0].split(sep=".")
+        size:int = len(IPDefaultValueList)
+        self.IPTextVariableList:list[StringVar] = [StringVar() for i in range(size)]
+        for i in range(size):
+            self.IPTextVariableList[i].set(value=IPDefaultValueList[i])
 
-print("Disconnecting from the server at {}".format(serverAddressStr))
-server.close()
+        self.inputTextVariable:StringVar = StringVar()
+        self.inputTextVariable.trace_add(mode="write", callback=self.updateSendButtonState)
+
+        self.setTitle(info=ClientWindow.DISCONNECTED)
+        self.createServerFrame(master=self, row=0, column=0)
+        self.createMessageFrame(master=self, row=1, column=0)
+
+
+    def isAbleToSend(self)->bool:
+        return self.isConnected and self.inputTextVariable.get() != ""
+    def updateSendButtonState(self, *paramList):
+        self.sendButton.config(state=NORMAL if self.isAbleToSend() else DISABLED)
+
+    def setTitle(self, info:str):
+        self.title(string="{} - {}".format(ClientWindow.BASE_TITLE, info))
+
+    def createFrame(self, row:int=None, column:int=None, sticky:str=None, side:str=None, padx:int=None, *paramList, **paramDict)->Frame:
+        frame:Frame = Frame(*paramList, **paramDict)
+        if side is not None or padx is not None:
+            frame.pack(side=side, padx=padx)
+        else:
+            frame.grid(row=row, column=column, sticky=sticky, padx=padx)
+        return frame
+
+    def displayMsg(self, msg:str):
+        self.showText.config(state=NORMAL)
+        self.showText.insert(index=END, chars=msg)
+        self.showText.config(state=DISABLED)
+        self.showText.see(index=END)
+
+    def listenServerThreadRunMethod(self):
+        while self.isConnected:
+            socketList, wList, xList = select([self.socket], [], [], TIMEOUT)
+            for socketWithMsg in socketList:
+                try:
+                    msgReceived:str = socketWithMsg.recv(1024).decode()
+                    addr:tuple[str,int] = socketWithMsg.getpeername()
+                    self.displayMsg(msg="<<{}\n".format(msgReceived))
+                    if msgReceived == STOP_SERVER:
+                        self.disconnection()
+                except ConnectionResetError:
+                    self.disconnection()
+
+    def connectionThreadRunMethod(self):
+        try:
+            for widget in self.serverConfigWidget:
+                widget.config(state="readonly")
+            self.connectButton.config(text=ClientWindow.CONNECTING, state=DISABLED)
+            self.setTitle(info=ClientWindow.CONNECTING)
+
+            self.socket:socket = createSocket()
+            self.socket.connect((self.ip, self.port))
+            
+            self.connectButton.config(text=ClientWindow.DISCONNECTION, command=self.disconnection, state=NORMAL)
+            self.setTitle(info=ClientWindow.CONNECTED)
+            self.isConnected = True
+            self.updateSendButtonState()
+
+            self.listenServerThread:Thread = Thread(target=self.listenServerThreadRunMethod)
+            self.listenServerThread.start()
+            self.inputTextEntry.focus()
+        except Exception as e:
+            print(e)
+            for widget in self.serverConfigWidget:
+                widget.config(state=NORMAL)
+            self.connectButton.config(text=ClientWindow.CONNECTION, state=NORMAL)
+            self.setTitle(info=ClientWindow.DISCONNECTED)
+            self.isConnected = False
+            self.updateSendButtonState()
+
+    def disconnection(self):
+        self.isConnected = False
+        if get_ident() != self.listenServerThread.ident:
+            self.listenServerThread.join()
+        self.socket.close()
+
+        self.setTitle(info=ClientWindow.DISCONNECTED)
+        self.connectButton.config(text=ClientWindow.CONNECTION, command=self.connection)
+        for widget in self.serverConfigWidget:
+            widget.config(state=NORMAL)
+        self.updateSendButtonState()
+
+    def connection(self):
+        self.ip:str = ".".join(textVariable.get() for textVariable in self.IPTextVariableList)
+        self.port:int = int(self.portTextVariable.get())
+        thread:Thread = Thread(target=self.connectionThreadRunMethod)
+        thread.start()
+
+    # Callbacks to check inputs for PORT/IP entries
+    def checkInputIsInt(self, input:str, max:int)->bool:
+        return input == "" or (input.isdigit() and int(input) >= 0 and int(input) <= max)
+    def checkPortInput(self, input:str)->bool:
+        return self.checkInputIsInt(input=input, max=65535)
+    def checkIPInput(self, input:str)->bool:
+        return self.checkInputIsInt(input=input, max=255)
+
+    def createPortFrame(self, master:Widget, side:str):
+        frame:Frame = self.createFrame(master=master, side=side, padx=50)
+        Label(master=frame, text="PORT: ", font=self.FONT).grid(row=0, column=0)
+        entry:Entry = Entry(master=frame, textvariable=self.portTextVariable, width=5, font=self.FONT, justify=RIGHT)
+        entry.grid(row=0, column=1)
+        entry.config(validate="key", validatecommand=(self.register(func=self.checkPortInput), "%P"))
+        self.serverConfigWidget.append(entry)
+
+    def createIPFrame(self, master:Widget, side:str):
+        frame:Frame = self.createFrame(master=master, side=side)
+        Label(master=frame, text="IP: ", font=self.FONT).grid(row=0, column=0)
+        for i in range(len(self.IPTextVariableList)):
+            entry:Entry = Entry(master=frame, textvariable=self.IPTextVariableList[i], width=3, font=self.FONT, justify=RIGHT)
+            entry.grid(row=0, column=1+i*2)
+            entry.config(validate="key", validatecommand=(self.register(func=self.checkIPInput), "%P"))
+            self.serverConfigWidget.append(entry)
+            if i != len(self.IPTextVariableList)-1:
+                Label(master=frame, text=".", font=self.FONT).grid(row=0, column=1+i*2+1)
+
+    def createServerFrame(self, master:Widget, row:int, column:int):
+        frame:Frame = self.createFrame(master=master, sticky=NSEW)
+        buttonWidth:int = max(len(ClientWindow.CONNECTION), len(ClientWindow.CONNECTING), len(ClientWindow.DISCONNECTION))
+        self.connectButton:Button = Button(master=frame, text=ClientWindow.CONNECTION, width=buttonWidth, command=self.connection, font=self.FONT)
+        self.connectButton.pack(side=RIGHT)
+        self.createPortFrame(master=frame, side=RIGHT)
+        self.createIPFrame(master=frame, side=RIGHT)
+
+    def createShowTextFrame(self, master:Widget, width:int, row:int, column:int):
+        frame:Frame = self.createFrame(master=master, row=row, column=column)
+        self.showText:Text = Text(master=frame, width=width-1, height=10, font=self.FONT, state=DISABLED)
+        self.showText.grid(row=0, column=0)
+        scroll = Scrollbar(master=frame, orient=VERTICAL, command=self.showText.yview)
+        scroll.grid(row=0, column=1, sticky=NS)
+        self.showText.config(yscrollcommand=scroll.set)
+
+    def sendMessage(self):
+        if self.isAbleToSend():
+            msgToSend:str = self.inputTextVariable.get()
+            self.socket.send(msgToSend.encode())
+            self.displayMsg(msg=">>{}\n".format(msgToSend))
+            self.inputTextVariable.set(value="")
+
+    def createInputTextFrame(self, master:Widget, width:int, row:int, column:int):
+        frame:Frame = self.createFrame(master=master, row=row, column=column)
+        buttonWidth:int = 5
+        self.inputTextEntry:Entry = Entry(master=frame, textvariable=self.inputTextVariable, width=width-buttonWidth, font=self.FONT)
+        self.inputTextEntry.grid(row=0, column=0)
+        self.sendButton = Button(master=frame, text="Send", command=self.sendMessage, width=buttonWidth, font=self.FONT, state=DISABLED)
+        self.sendButton.grid(row=0, column=1)
+        self.inputTextEntry.bind(sequence="<Return>", func=lambda event:self.sendButton.invoke())
+        
+    def createMessageFrame(self, master:Widget, row:int, column:int):
+        frame:Frame = self.createFrame(master=master, row=row, column=column)
+        width:int = 75
+        self.createShowTextFrame(master=frame, width=width, row=0, column=0)
+        self.createInputTextFrame(master=frame, width=width, row=1, column=0)
+
+window:ClientWindow = ClientWindow()
+window.mainloop()
--- Server/Server.py
+++ Server/Server.py
@@ -5,39 +5,52 @@ from select import select
 
 MSG_CLIENT_DISCONNECTION:str = ""
 
-server:socket = createSocket()
-server.bind(serverAddress)
-server.listen(5)
-print("Server ready at {}".format(serverAddressStr))
+try:
+    server:socket = createSocket()
+    server.bind(serverAddress)
+    server.listen(5)
+    print("Server ready at {}".format(serverAddressStr))
 
-clientList:list[socket] = []
-serverLoop:bool = True
-while serverLoop:
-    rlist, wList, xList = select([server], [], [], TIMEOUT)
-    for newConnection in rlist:
-        client, addr = newConnection.accept()
-        print("Client connected: {}:{}".format(*addr))
-        clientList.append(client)
+    clientList:list[socket] = []
+    serverLoop:bool = True
+    while serverLoop:
+        rlist, wList, xList = select([server], [], [], TIMEOUT)
+        for newConnection in rlist:
+            client, addr = newConnection.accept()
+            print("Client connected: {}:{}".format(*addr))
+            clientList.append(client)
 
-    if len(clientList) > 0:
-        rlist, wList, xList = select(clientList, [], [], TIMEOUT)
-        for client in rlist:
-            msgReceived:str = client.recv(1024).decode()
-            addr:tuple[str,int] = client.getpeername()
-            if msgReceived == MSG_CLIENT_DISCONNECTION:
-                print("Client disconnected: {}:{}".format(*addr))
-                client.close()
-                clientList.remove(client)
-            elif msgReceived == STOP_SERVER:
-                serverLoop = False
-            else:
-                print("Message received from the client at {}:{}:\n\t{}".format(*addr, msgReceived))
-                client.send(checkInput(prompt="Reply to message from the client at {}:{}:\n\t_".format(*addr)).encode())
+        if len(clientList) > 0:
+            rlist, wList, xList = select(clientList, [], [], TIMEOUT)
+            for client in rlist:
+                try:
+                    msgReceived:str = client.recv(1024).decode()
+                    addr:tuple[str,int] = client.getpeername()
+                    if msgReceived == MSG_CLIENT_DISCONNECTION:
+                        print("Client disconnected: {}:{}".format(*addr))
+                        client.close()
+                        clientList.remove(client)
+                    elif msgReceived == STOP_SERVER:
+                        serverLoop = False
+                    else:
+                        print("Message received from the client at {}:{}:\n\t{}".format(*addr, msgReceived))
+                        client.send(checkInput(prompt="Reply to message from the client at {}:{}:\n\t_".format(*addr)).encode())
+                except Exception as e:
+                    print(e)
 
-for client in clientList:
-    client.send(STOP_SERVER.encode())
-    client.close()
-clientList.clear()
+    for client in clientList:
+        client.send(STOP_SERVER.encode())
+        client.close()
+    clientList.clear()
 
-print("Server shutdown {}".format(serverAddress))
-server.close()
+    print("Server shutdown {}".format(serverAddressStr))
+    server.close()
+except:
+    try:
+        for client in clientList:
+            client.send(STOP_SERVER.encode())
+            client.close()
+        clientList.clear()
+    finally:
+        print("Server shutdown {}".format(serverAddressStr))
+        server.close()
