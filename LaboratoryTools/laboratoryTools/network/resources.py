ede10503bcaef58ab782f6f30e966bbe2f89f686
Gillou <gilles.hubert97@gmail.com>
2022-02-21 17:28:57 +0100

    [laboratoryTools.network] - Move resources in a json file.

--- LaboratoryTools/laboratoryTools/network/resources.py
+++ LaboratoryTools/laboratoryTools/network/resources.py
@@ -1,3 +1,19 @@
-# Server constant
-PORT:"int" = 12800
-PASSWORD:"str" = "PASSWORD"
+from laboratoryTools.logging import logger, displayError
+from os.path import dirname, join
+from json import loads
+
+filePath:"str" = join(dirname(__file__), "resources.json")
+
+content:"str" = ""
+with open(filePath, "r") as file:
+    content = file.read()
+
+resources:"dict[str,any]" = loads(s=content)
+PORT:"int" = resources["PORT"]
+PASSWORD:"str" = resources["PASSWORD"]
+
+if __name__ == "__main__":
+    try:
+        pass
+    except Exception as e:
+        logger.error(msg=displayError(error=e))
